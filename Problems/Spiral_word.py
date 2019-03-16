b157a6033b18c7710b84c82200e26d09f4ce9dbb
Servillian <47287191+Servillian@users.noreply.github.com>
2019-03-16 12:13:58 +1100

    giv m da plant

--- Problems/Spiral_word.py
+++ Problems/Spiral_word.py
@@ -2,10 +2,14 @@ def spiral_word(word):
     #Checking no duplicates
     if len(set(word)) != len(word):
         return False
-    for i in
-            print("very nice")
+    a = -1
+    result = list(word[0])
+    for i in range(1,len(word)//2+1):
+        result.append(word[i*a])
+        if i == len(word)//2:
+            break
+        result.append(word[i])
+    return result == sorted(result, reverse=True) or result == sorted(result, reverse=False)
 
 
-print(spiral_word("urgent"))
-
-#for i in range(len(word) - 1): if word[] > word[i + 1]: return True else:return False
+print(spiral_word("all"))
